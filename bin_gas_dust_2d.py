7f15ebff9624679a0f27d7a966c520fd544e2e07
Alexander Kaurov <kaurov@ias.edu>
2018-08-01 17:29:14 -0400

    movie maker

--- bin_gas_dust_2d.py
+++ bin_gas_dust_2d.py
@@ -6,39 +6,113 @@ import matplotlib.pyplot as plt
 import random
 from astropy.io import fits
 
-dust=gadget.readsnap('/panfs/ds08/hopkins/phopkins/data2/RDI_dust_spectrum/HR/s.k05N4.HR/output/', 0, 3)
-gas=gadget.readsnap('/panfs/ds08/hopkins/phopkins/data2/RDI_dust_spectrum/HR/s.k05N4.HR/output/', 0, 0)
-#dust=gadget.readsnap('/panfs/ds08/hopkins/uli/RDI_dust_spectrum/LR/output_4HR/', 0, 3)
 
+biases = []
+data = []
+steps = np.arange(0,200.1,5).astype('int')
+for s in steps:
+    dust=gadget.readsnap('/panfs/ds08/hopkins/phopkins/data2/RDI_dust_spectrum/HR/s.k05N4.HR/output/', s, 3)
+    gas=gadget.readsnap('/panfs/ds08/hopkins/phopkins/data2/RDI_dust_spectrum/HR/s.k05N4.HR/output/', s, 0)
+    #dust=gadget.readsnap('/panfs/ds08/hopkins/uli/RDI_dust_spectrum/LR/output_4HR/', 0, 3)
+    #
+    dust_pos=dust['p']
+    grainsize=dust['GrainSize']
+    dust_mass=dust['m']
+    x_dust=dust_pos[:,0]
+    y_dust=dust_pos[:,2]
+    #
+    gas_pos=gas['p']
+    x_gas=gas_pos[:,0]
+    y_gas=gas_pos[:,2]
+    gas_mass=gas['m']
+    #
+    bins=np.linspace(0,1,32)
+    weight_dust=dust_mass/(1/64. * 1/64.)
+    weight_gas=gas_mass/(1/64. * 1/64.)
+    #
+    h_dust=np.histogram2d(x_dust,y_dust,bins=bins, weights=weight_dust)[0]
+    h_gas=np.histogram2d(x_gas, y_gas, bins=bins, weights=weight_gas)[0]
+    #
+    cbins=(bins[:1]+bins[:-1])/2
+    #
+    mean_dust_density=np.mean(h_dust)
+    mean_gas_density=np.mean(h_gas)
+    h_dust = (h_dust-mean_dust_density)/mean_dust_density
+    h_gas = (h_gas-mean_gas_density)/mean_gas_density
+    print s
+    print np.mean(h_dust)
+    print np.mean(h_gas)
+    print 'bias=', np.mean(h_gas/h_dust)
+    biases.append(np.mean(h_gas/h_dust))
+    data.append([h_gas, h_dust])
+
+
+plt.plot(steps, biases)
+plt.show()
+
+
+for i in [-1]:#len(data):
+    plt.figure(figsize=(8,8))
+    plt.subplot(221)
+    plt.imshow(data[i][0])
+    plt.axis('off')
+    plt.subplot(222)
+    plt.imshow(data[i][1])
+    plt.axis('off')
+    plt.subplot(223)
+    plt.imshow(data[i][0]/data[i][1])
+    print(np.mean(data[i][0]/data[i][1]), np.median(data[i][0]/data[i][1]))
+    plt.axis('off')
+
+
+plt.show()
+
+biases_med = []
+for i in range(len(data)):
+    biases_med.append(np.median(data[i][0] / data[i][1]))
+
+plt.plot(steps/10., biases_med)
+plt.ylabel('bias = median($\delta_g / \delta_d$)')
+plt.xlabel('t')
+plt.ylim([-0.05, 0.1])
+plt.show()
+
+import h5py as h5py
+import numpy as np
+import gadget
+import math
+import matplotlib.pyplot as plt
+import random
+from astropy.io import fits
+
+bins=np.linspace(0,1,64)
+s=120
+
+p = '/panfs/ds08/hopkins/phopkins/data2/RDI_dust_spectrum/HR/s.k05N4.HR/output/'
+
+dust=gadget.readsnap(p, s, 3)
+gas=gadget.readsnap(p, s, 0)
+#dust=gadget.readsnap('/panfs/ds08/hopkins/uli/RDI_dust_spectrum/LR/output_4HR/', 0, 3)
+#
 dust_pos=dust['p']
 grainsize=dust['GrainSize']
 dust_mass=dust['m']
 x_dust=dust_pos[:,0]
-y_dust=dust_pos[:,1]
-
+y_dust=dust_pos[:,2]
+#
 gas_pos=gas['p']
 x_gas=gas_pos[:,0]
-y_gas=gas_pos[:,1]
+y_gas=gas_pos[:,2]
 gas_mass=gas['m']
 
-bins=np.linspace(0,1,64)
-weight_dust=dust_mass/(1/64. * 1/64.)
-weight_gas=gas_mass/(1/64. * 1/64.)
+h_dust = np.histogram2d(x_dust, y_dust, bins=bins, )[0]
 
-h_dust=np.histogram2d(x_dust,y_dust,bins=bins, weights=weight_dust)[0]
-h_gas=np.histogram2d(x_gas, y_gas, bins=bins, weights=weight_gas)[0]
+h_gas = np.histogram2d(x_gas, y_gas, bins=bins,)[0]
 
-cbins=(bins[:1]+bins[:-1])/2
 
-mean_dust_density=np.mean(h_dust)
-mean_gas_density=np.mean(h_gas)
-h_dust*=(h_dust-mean_dust_density)/mean_dust_density
-h_gas*=(h_gas-mean_gas_density)/mean_gas_density
+plt.imshow(h_gas)
+plt.show()
 
-print np.mean(h_dust)
-print np.mean(h_gas)
-
-print 'bias=', np.mean(h_gas)/np.mean(h_dust) 
 
 #hdu = fits.PrimaryHDU()
 #hdu.data=array
---
+++ clean_64_plot.py
@@ -0,0 +1,95 @@
+import h5py as h5py
+import numpy as np
+import gadget
+import math
+import matplotlib.pyplot as plt
+import random
+from astropy.io import fits
+from matplotlib.colors import LogNorm
+
+path1 = '/panfs/ds08/hopkins/uli/RDI_dust_spectrum/HR/s.k05N4.HR/output_dust_grid_gas_grid'
+
+path = path1
+
+biases = []
+data = []
+steps = np.arange(0,200.1,1).astype('int')
+
+cyls = []
+ncyls = 32
+dust_norm = 0.011983339715927964*2.
+
+
+bins = np.linspace(0, 1, 32)
+cbins = (bins[:1] + bins[:-1]) / 2
+
+xm, ym = np.meshgrid(cbins, cbins)
+
+# steps=[0]
+plt.figure()
+
+for s in steps:
+    print s
+    dust=gadget.readsnap(path, s, 3)
+    gas=gadget.readsnap(path, s, 0)
+    #dust=gadget.readsnap('/panfs/ds08/hopkins/uli/RDI_dust_spectrum/LR/output_4HR/', 0, 3)
+    #
+    dust_pos=dust['p']
+    grainsize=dust['GrainSize']
+    dust_mass=dust['m']
+    x_dust=dust_pos[:,0]
+    y_dust=dust_pos[:,2]
+    #
+    gas_pos=gas['p']
+    x_gas=gas_pos[:,0]
+    y_gas=gas_pos[:,2]
+    gas_mass=gas['m']
+    #
+    weight_dust = dust_mass**(2./3)
+    weight_gas=gas_mass
+    #
+    h_dust=np.histogram2d(x_dust,y_dust,bins=bins, weights=weight_dust)[0]
+    h_gas=np.histogram2d(x_gas, y_gas, bins=bins, weights=weight_gas)[0]
+    plt.clf()
+    plt.subplot(221)
+    plt.imshow(np.log10(h_gas), vmin=np.log10(0.0008), vmax=np.log10(0.0013))
+    plt.title('%.1d'%(0.1*s))
+    plt.axis('off')
+    plt.subplot(222)
+    plt.imshow(np.log10(h_dust), vmin=np.log10(0.007), vmax=np.log10(0.02))
+    plt.axis('off')
+    temp = []
+    filt = ((xm-0.5)**2 + (ym-0.5)**2)<0.2**2
+    cyls.append(-np.log(1.0-np.mean(1.0 - np.exp( - h_dust[filt] / dust_norm))))
+    #
+    mean_dust_density=np.mean(h_dust)
+    mean_gas_density=np.mean(h_gas)
+    h_dust = (h_dust-mean_dust_density)/mean_dust_density
+    h_gas = (h_gas-mean_gas_density)/mean_gas_density
+    print s
+    print np.mean(h_dust)
+    print np.mean(h_gas)
+    print 'bias=', np.median(h_gas/h_dust)
+    biases.append(np.median(h_gas/h_dust))
+    data.append([h_gas, h_dust])
+    plt.subplot(223)
+    plt.imshow(h_gas/h_dust, vmin=-3, vmax=3)
+    plt.axis('off')
+    plt.subplot(224)
+    plt.hist2d(h_gas.flatten(), h_dust.flatten(), bins=np.linspace(-1,1,32), norm=LogNorm())
+    plt.xlabel('gas overdensity')
+    plt.xlabel('dust overdensity')
+    plt.savefig('/home/kaurov/%03d.png'%s)
+
+# command to convert frames into video
+# ffmpeg -framerate 10 -i %03d.png -vcodec libx264 video.mp4
+
+x = np.linspace(0,20,201)
+plt.subplot(211)
+plt.plot(x, cyls)
+plt.ylabel('$\tau$')
+plt.subplot(212)
+plt.plot(x, biases)
+plt.ylabel('$b$')
+plt.xlabel('$t$')
+plt.show()
