d6ab3774f2dc02046ffddf56c6cbbac9ac240a91
Alexander Kaurov <kaurov@ias.edu>
2018-08-01 20:10:50 -0400

    upd

--- clean_64_plot.py
+++ clean_64_plot.py
@@ -21,7 +21,7 @@ dust_norm = 0.011983339715927964*2.
 
 
 bins = np.linspace(0, 1, 32)
-cbins = (bins[:1] + bins[:-1]) / 2
+cbins = (bins[1:] + bins[:-1]) / 2
 
 xm, ym = np.meshgrid(cbins, cbins)
 
@@ -57,10 +57,15 @@ for s in steps:
     plt.axis('off')
     plt.subplot(222)
     plt.imshow(np.log10(h_dust), vmin=np.log10(0.007), vmax=np.log10(0.02))
+    circle = plt.Circle((15, 15), 0.1*32, color='r', fill=False)
+    plt.gca().add_artist(circle)
     plt.axis('off')
     temp = []
-    filt = ((xm-0.5)**2 + (ym-0.5)**2)<0.2**2
-    cyls.append(-np.log(1.0-np.mean(1.0 - np.exp( - h_dust[filt] / dust_norm))))
+    for xc in [0.2, 0.4, 0.6, 0.8]:
+        for yc in [0.2, 0.4, 0.6, 0.8]:
+            filt = ((xm-xc)**2 + (ym-yc)**2)<0.1**2
+            temp.append(-np.log(1.0-np.mean(1.0 - np.exp( - h_dust[filt] / dust_norm))))
+    cyls.append(temp)
     #
     mean_dust_density=np.mean(h_dust)
     mean_gas_density=np.mean(h_gas)
@@ -73,12 +78,12 @@ for s in steps:
     biases.append(np.median(h_gas/h_dust))
     data.append([h_gas, h_dust])
     plt.subplot(223)
-    plt.imshow(h_gas/h_dust, vmin=-3, vmax=3)
+    plt.imshow(h_gas/h_dust, vmin=-3, vmax=3, cmap='bwr')
     plt.axis('off')
     plt.subplot(224)
     plt.hist2d(h_gas.flatten(), h_dust.flatten(), bins=np.linspace(-1,1,32), norm=LogNorm())
     plt.xlabel('gas overdensity')
-    plt.xlabel('dust overdensity')
+    plt.ylabel('dust overdensity')
     plt.savefig('/home/kaurov/%03d.png'%s)
 
 # command to convert frames into video
@@ -86,8 +91,12 @@ for s in steps:
 
 x = np.linspace(0,20,201)
 plt.subplot(211)
-plt.plot(x, cyls)
-plt.ylabel('$\tau$')
+plt.plot(x, np.array(cyls), alpha=0.5)
+m = np.array(cyls).mean(1)
+s = np.array(cyls).std(1)
+plt.plot(x, m, 'k')
+plt.fill_between(x, m-s, m+s, alpha=0.5)
+plt.ylabel(r'$\tau$')
 plt.subplot(212)
 plt.plot(x, biases)
 plt.ylabel('$b$')
